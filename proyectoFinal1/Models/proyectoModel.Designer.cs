8ef2d08444ae45504358947913eb25a6c3815e34
Ludswing Michael Cahuec Ojom <ludswingc@gmail.com>
2018-04-20 20:00:52 -0600

    ultimos cambios

--- proyectoFinal1/Models/proyectoModel.Designer.cs
+++ proyectoFinal1/Models/proyectoModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\spectre\documents\visual studio 2017\Projects\proyectoFinal1\proyectoFinal1\Models\proyectoModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\spectre\Documents\Visual Studio 2017\Projects\proyectoFinal1\proyectoFinal1\Models\proyectoModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
